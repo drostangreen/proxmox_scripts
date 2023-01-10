55fa2c24bf436679ca2b4474c799ffa03b9f5c48
drostangreen <t.w.green29@gmail.com>
2023-01-10 10:26:03 -0600

    Cleaned up messages

--- template_create.sh
+++ template_create.sh
@@ -62,30 +62,30 @@ image=$distro.qcow2
 echo "Downloading $image now"; wget -qO $image $url
 
 # installing qemu-guest-agent into the downloaded qcow2 image, then removing machine-id
-virt-customize -a $image --update
-virt-customize -a $image --install qemu-guest-agent
-virt-customize -a $image --run-command '>/etc/machine-id'
+echo "Updating $image"; virt-customize -a $image --update > /dev/null
+echo "Installing qemu-guest-agent"; virt-customize -a $image --install qemu-guest-agent > /dev/null
+echo "Removing /etc/machine-id"; virt-customize -a $image --run-command '>/etc/machine-id' > /dev/null
 
 # create VM, resize the disk, import disk and create Cloud Init drive. Then set default settings for vm
-qm create $VMID --memory 1024 --core 1 --name $distro-template-ga --net0 virtio,bridge=vmbr0
-qemu-img resize $image 10G
-qm importdisk $VMID $image $storage --format qcow2
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$VMID-disk-0,discard=on,ssd=1
+echo "Creating $VMID with name $distro-template-ga"; qm create $VMID --memory 1024 --core 1 --name $distro-template-ga --net0 virtio,bridge=vmbr0 > /dev/null
+echo "Resizing disk"; qemu-img resize $image 10G > /dev/null
+echo "Importing disk"; qm importdisk $VMID $image $storage --format qcow2 > /dev/null
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$VMID-disk-0,discard=on,ssd=1 > /dev/null
 if [ $? == 0 ]; then
     echo "Setting Virtual Drive to scsi0"
 else
     echo "Setting Virtual Drive to scsi0 on shared storage"; qm set $VMID --scsihw virtio-scsi-pci --scsi0 $storage:$VMID/vm-$VMID-disk-0.qcow2,discard=on,ssd=1,format=qcow2 > /dev/null
 fi
-qm set $VMID --ide2 local-lvm:cloudinit
-qm set $VMID --boot c --bootdisk scsi0
-qm set $VMID --serial0 socket --vga serial0
-qm set $VMID --agent 1
+echo "Add Cloud Init drive"; qm set $VMID --ide2 local-lvm:cloudinit > /dev/null
+echo "Set bootdisk to scsi0"; qm set $VMID --boot c --bootdisk scsi0 > /dev/null
+echo "Set serial connecton"; qm set $VMID --serial0 socket --vga serial0 > /dev/null
+echo "Enabling qemu-guest-agent in Proxmox"; qm set $VMID --agent 1 > /dev/null
 
 # Below is Cloud Init. Switch the Commented line if you'd like to set a password.
 # The Proxmox team does recommend only using SSH key and now password. You will not be able to 
 # log in from the console in this case.
-# qm set $VMID --ciuser tadmin --cipassword $cipassword --ipconfig0 ip=dhcp --sshkeys $sshkeys
-qm set $VMID --ciuser tadmin --ipconfig0 ip=dhcp --sshkeys $sshkeys
+# echo "Defining Cloud Init params"; qm set $VMID --ciuser tadmin --cipassword $cipassword --ipconfig0 ip=dhcp --sshkeys $sshkeys > /dev/null
+echo "Defining Cloud Init params"; qm set $VMID --ciuser tadmin --ipconfig0 ip=dhcp --sshkeys $sshkeys > /dev/null
 
 # Comment this line to skip the pause to check settings
 pause 'Press [Enter] key to create finish creating template...'
