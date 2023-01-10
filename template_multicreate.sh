1c3994e2e38335066854ab4b52eea0f2a12adb6d
Tristan Green <tristan@INTERNAL.LAN>
2023-01-10 10:07:37 -0600

    Import as qcow2

--- template_multicreate.sh
+++ template_multicreate.sh
@@ -69,12 +69,12 @@ template_create () {
     # create VM, resize the disk, import disk and create Cloud Init drive. Then set default settings for vm
     echo "Creating $VMID with name ${distro[$index]}-template-ga"; qm create $VMID --memory 1024 --core 1 --name ${distro[$index]}-template-ga --net0 virtio,bridge=vmbr0 > /dev/null
     echo "Resizing disk"; qemu-img resize $image 10G > /dev/null
-    echo "Importing disk"; qm importdisk $VMID $image $storage > /dev/null
+    echo "Importing disk"; qm importdisk $VMID $image $storage --format qcow2 > /dev/null
     qm set $VMID --scsihw virtio-scsi-pci --scsi0 $storage:vm-$VMID-disk-0,discard=on,ssd=1 > /dev/null
     if [ $? == 0 ]; then
         echo "Setting Virtual Drive to scsi0"
     else
-        echo "Setting Virtual Drive to scsi0 on shared storage"; qm set $VMID --scsihw virtio-scsi-pci --scsi0 $storage:$VMID/vm-$VMID-disk-0.raw,discard=on,ssd=1,format=qcow2 > /dev/null
+        echo "Setting Virtual Drive to scsi0 on shared storage"; qm set $VMID --scsihw virtio-scsi-pci --scsi0 $storage:$VMID/vm-$VMID-disk-0.qcow2,discard=on,ssd=1,format=qcow2 > /dev/null
     fi
     echo "Add Cloud Init drive"; qm set $VMID --ide2 $storage:cloudinit > /dev/null
     echo "Set bootdisk to scsi0"; qm set $VMID --boot c --bootdisk scsi0 > /dev/null
