9fed54c1f361447180c5877e5932ee0136300dbc
drostangreen <t.w.green29@gmail.com>
2023-01-27 15:48:19 -0600

    reworked array

--- template_multicreate.sh
+++ template_multicreate.sh
@@ -6,30 +6,6 @@ Help(){
     echo "-s    select storage target (use shared storage if available) *REQUIRED"
 }
 
-# Place the name of the distros you would like to see created
-# Order matters they must match up one for one
-# Below is what I use but you can modify it to your liking
-distro=(
-    debian10
-    debian11
-    ubuntu2004
-    ubuntu2204
-    rocky8
-    rocky9
-    fedora37
-    opensuse154
-)
-url=(
-    https://cloud.debian.org/images/cloud/buster/latest/debian-10-generic-amd64.qcow2
-    https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2
-    https://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64.img
-    https://cloud-images.ubuntu.com/minimal/releases/jammy/release/ubuntu-22.04-minimal-cloudimg-amd64.img
-    https://dl.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud-Base.latest.x86_64.qcow2
-    https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2
-    https://download.fedoraproject.org/pub/fedora/linux/releases/37/Cloud/x86_64/images/Fedora-Cloud-Base-37-1.7.x86_64.qcow2
-    https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.4/images/openSUSE-Leap-15.4.x86_64-1.0.1-NoCloud-Build2.188.qcow2
-)
-
 while getopts hv:s: flag
 do
     case $flag in
@@ -54,6 +30,17 @@ do
     esac
 done
 
+declare -A distro_array=(
+    [debian10]="https://cloud.debian.org/images/cloud/buster/latest/debian-10-generic-amd64.qcow2"
+    [debian11]="https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2"
+    [ubuntu2004]="https://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64.img"
+    [ubuntu2204]="https://cloud-images.ubuntu.com/minimal/releases/jammy/release/ubuntu-22.04-minimal-cloudimg-amd64.img"
+    [rocky8]="https://dl.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud-Base.latest.x86_64.qcow2"
+    [rocky9]="https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
+    [fedora37]="https://download.fedoraproject.org/pub/fedora/linux/releases/37/Cloud/x86_64/images/Fedora-Cloud-Base-37-1.7.x86_64.qcow2"
+    [opensuse154]="https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.4/images/openSUSE-Leap-15.4.x86_64-1.0.1-NoCloud-Build2.188.qcow2"
+)
+
 echo "Checking for required package"; which virt-customize > /dev/null
 if [ $? -eq 0 ]; then
     echo "libguestfs-tools is already installed... Continuing..."
@@ -66,7 +53,7 @@ echo "Removing any old qcow2 or img files"; rm -f *.qcow2 *.img > /dev/null
 template_create () {
     sshkeys=~/tadmin_pub_key
     
-    echo "Downloading $image now"; wget -qO $image ${url[$index]}
+    echo "Downloading $image from $url"; wget -qO $image $url
 
     # installing qemu-guest-agent into the downloaded qcow2 image, then removing machine-id
     echo "Install qemu-guest-agent on first boot"; virt-customize -a $image --firstboot-install qemu-guest-agent > /dev/null
@@ -74,7 +61,7 @@ template_create () {
     echo "Removing /etc/machine-id"; virt-customize -a $image --run-command '>/etc/machine-id' > /dev/null
 
     # create VM, resize the disk, import disk and create Cloud Init drive. Then set default settings for vm
-    echo "Creating $VMID with name ${distro[$index]}-template-ga"; qm create $VMID --memory 1024 --core 1 --name ${distro[$index]}-template-ga --net0 virtio,bridge=vmbr0 > /dev/null
+    echo "Creating $VMID with name $distro-template-ga"; qm create $VMID --memory 1024 --core 1 --name $distro-template-ga --net0 virtio,bridge=vmbr0 > /dev/null
     echo "Resizing disk"; qemu-img resize $image 10G > /dev/null
     echo "Importing disk"; qm importdisk $VMID $image $storage --format qcow2 > /dev/null
     if [ -f /mnt/pve/$storage/images/$VMID/*-$VMID-disk-0.qcow2 ]; then
@@ -98,10 +85,11 @@ template_create () {
     echo "Deleting $image file"; rm $image > /dev/null
 } 
 
-for index in ${!distro[*]};
+for distro in ${!distro_array[*]};
 do
     ((VMID=VMID+1))
-    image=${distro[$index]}.qcow2
+    image=$distro.qcow2
+    url=${distro_array[$distro]}
     template_create
 done
 
